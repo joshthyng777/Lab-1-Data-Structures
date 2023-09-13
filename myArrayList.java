8942ae90d61c578ba841a992f2a59d52c2afc041
Josh <joshthyng187@gmail.com>
2023-09-13 14:36:12 -0400

    myArraryList

---
+++ myArrayList.java
@@ -0,0 +1,7 @@
+public class myArrayList<T> {
+    private static final int INITIAL_CAPACITY = 50;
+    private static final double GROWTH_FACTOR = 1.2;
+
+    private Object[] elements;
+    private int size;
+}
