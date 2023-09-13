98262467c0eca6d49ee49cb643849a2e3b94514f
Josh <joshthyng187@gmail.com>
2023-09-13 14:22:41 -0400

    added divide method

--- Fraction.java
+++ Fraction.java
@@ -2,12 +2,12 @@
 public class Fraction implements INumber<Object> {
     int numerator;
     int denominator;
-    
+
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
-    
+
     /**
      * @author Ellis Fitzgerald
      * @version 12 September 2023
@@ -15,14 +15,13 @@ public class Fraction implements INumber<Object> {
      * @return boolean - If the parameter one and parameter two are the same value
      */
     public boolean hasSameValue(Fraction fraction) {
-        if(fraction.numerator == 0 || fraction.denominator == 0) {
+        if (fraction.numerator == 0 || fraction.denominator == 0) {
             return false;
-        }
-        else {
+        } else {
             boolean isEqual = (this.numerator * fraction.denominator) == (fraction.numerator * this.denominator);
             return isEqual;
         }
-      
+
     }
 
     @Override
@@ -30,10 +29,10 @@ public class Fraction implements INumber<Object> {
      * @author Ellis Fitzgerald
      * @version 13 September 2023
      * @param input - An object that is casted to a Fraction
-     * plus - Adds this Fraction with input Fraction
+     *              plus - Adds this Fraction with input Fraction
      */
     public Object plus(Object input) {
-        Fraction frac = (Fraction)input;
+        Fraction frac = (Fraction) input;
         int sumNumerator = (this.numerator * frac.denominator) + (frac.numerator * this.denominator);
         int sumDenominator = this.denominator * frac.denominator;
         return new Fraction(sumNumerator, sumDenominator);
@@ -44,19 +43,24 @@ public class Fraction implements INumber<Object> {
      * @author Ellis Fitzgerald
      * @version 13 September 2023
      * @param input - An object that is casted to a Fraction
-     * minus - Subtracts this Fraction with input Fraction
+     *              minus - Subtracts this Fraction with input Fraction
      */
     public Object minus(Object input) {
-        Fraction frac = (Fraction)input;
+        Fraction frac = (Fraction) input;
         int subNumerator = (this.numerator * frac.denominator) - (frac.numerator * this.denominator);
         int subDenominator = this.denominator * frac.denominator;
         return new Fraction(subNumerator, subDenominator);
     }
 
+    //Divide method
     @Override
     public Object divide(Object input) {
-        // TODO Auto-generated method stub
-        return null;
+        Fraction frac = (Fraction) input;
+
+        int divNumerator = this.numerator * frac.denominator;
+        int divDenominator = this.denominator * frac.numerator;
+
+        return new Fraction(divNumerator, divDenominator);
     }
 
     @Override
@@ -64,11 +68,11 @@ public class Fraction implements INumber<Object> {
      * @author Ellis Fitzgerald
      * @version 13 September 2023
      * @param input - An object that is casted to a Fraction
-     * multiply - Multiplies this Fraction with input Fraction
+     *              multiply - Multiplies this Fraction with input Fraction
      * 
      */
     public Object multiply(Object input) {
-        Fraction frac = (Fraction)input;
+        Fraction frac = (Fraction) input;
         int productNumerator = this.numerator * frac.numerator;
         int productDenominator = this.denominator * frac.denominator;
         return new Fraction(productNumerator, productDenominator);
@@ -77,6 +81,6 @@ public class Fraction implements INumber<Object> {
     @Override
     public void print() {
         // TODO Auto-generated method stub
-        
+
     }
 }
